51de88f93483436b2fa0881aeee8801355c1f8bf
Connor Kelly <connor.r.kelly@gmail.com>
2013-01-16 12:06:27 -0500

    Create map function
    The map function walks a directory and populates a set of tree objects
    to create a directory tree.

--- articles.py
+++ articles.py
@@ -12,7 +12,30 @@ class tree(object):
 		url = os.path.relpath(self.path, prefix)
 		self.url = '.'.join(url.split(sep))
 		self.base = os.path.basename(self.path)
-	
+
+def map(contentdir):
+	sep = pathsep()
+	map = []
+	root = tree()
+	root.path = contentdir
+	root.webpaths()
+	map.append(root)
+	dict = {root.path:root}
+	cur_node = root
+	for(path, dirs, files) in os.walk(contentdir):
+		cur_node = dict[path]
+		mds = filter(lambda x: 'md' == x[-2:], files)
+		for file in files:
+			webfile = ' '.join(file.split('.')[:-1]).title()
+			cur_node.files.append((file, webfile))
+		for d in dirs:
+			t = tree()
+			t.path = cur_node.path + sep + d
+			t.webpaths()
+			dict.update({t.path:t})
+			cur_node.dirs.append(t)
+	return map	
+
 def pathsep():
 	sep = '/'
 	if(os.name == 'nt'):
