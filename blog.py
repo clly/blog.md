94ce180a42eb2239cef67e76f43a84ad1e174ad4
Connor Kelly <connor.r.kelly@gmail.com>
2013-01-16 12:15:58 -0500

    Create map function and recursive loop in template
    The map function walks a directory and populates a set of tree objects
    to create a directory tree.
    
    The template needs a specificly created recursive loop to correctly
    create the navigation tree.  The loop starts on the first set of
    directories in map.

--- blog.py
+++ blog.py
@@ -10,12 +10,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	file = 'static/md/index.md'
+	prefix = os.path.normpath('static/md/articles')
+	map = articles.map(prefix)
 	if(os.path.exists(file)):
 		html = mark.convert(file)
 	else:
 		abort(404)
 	title = html.metadata['title']
-	return render_template('def.html', html=html, title=title)
+	return render_template('def.html', map=map, html=html, title=title)
 
 @app.route('/article/<pth>/<mdfile>')
 def article(pth = None, mdfile = None):
